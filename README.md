50fa0cf07be0f809369a4c62f7baaaa7d12eb1e1
theinvestorapp <theinvestorapp@protonmail.com>
2021-05-19 09:58:32 +0200

    trabajo spring boot Max

---
+++ README.md
@@ -0,0 +1,2 @@
+# trabajoEntornosDesarrolloFramework
+Trabajo de la tercera evaluación de Entornos de Desarrollo para trabajar con el framework Springboot y java 
---
+++ src/test/java/com/TrabajoEntornos/trabajoEntornos/TrabajoEntornosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrabajoEntornos.trabajoEntornos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoEntornosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
